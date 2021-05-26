65204ea6d15a3262fdb88ece86311e3ccbd7ec50
cujo55575 <cujo55575@gmail.com>
2021-05-26 21:16:08 +0630

    add salt

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const ejs=require('ejs');
 const mongoose=require('mongoose');
-const md5=require('md5');
-// const encrypt=require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app=express();
 
@@ -26,12 +26,28 @@ const userSchema=new mongoose.Schema({
 });
 
 
-// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
-
 const User=new mongoose.model("User",userSchema);
 
 app.get('/',function(req,res){
     res.render('home');
+});
+
+app.get('/register',function(req,res){
+    res.render('register');
+});
+
+app.post('/register',function(req,res){
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      const newUser=new User({
+        username:req.body.username,
+        password:hash
+      });
+      newUser.save(function(err){
+        if(err)console.log(err);
+        else res.render('secrets');
+      });
+  });
 });
 
 app.get('/login',function(req,res){
@@ -40,29 +56,16 @@ app.get('/login',function(req,res){
 
 app.post('/login',function(req,res){
     const username=req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
     User.findOne({username:username},function(err,foundUser){
           if(err)console.log(err);
           else{
             if(foundUser){
-              if(foundUser.password===password)res.render('secrets');
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result)res.render('secrets');
+                });
             }
           }
-    });
-});
-
-app.get('/register',function(req,res){
-    res.render('register');
-});
-
-app.post('/register',function(req,res){
-    const newUser=new User({
-      username:req.body.username,
-      password:md5(req.body.password)
-    });
-    newUser.save(function(err){
-      if(err)console.log(err);
-      else res.render('secrets');
     });
 });
 
