6e3d0c9d1b1db32120c6f9d0180e8096a7f51951
cujo55575 <cujo55575@gmail.com>
2021-05-26 19:42:02 +0630

    Add evnironment vars

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
-//jshint esversion:6
+//jshint esversion:8
+require('dotenv').config();
+const express=require('express');
+const bodyParser=require('body-parser');
+const ejs=require('ejs');
+const mongoose=require('mongoose');
+const encrypt=require('mongoose-encryption');
+
+const app=express();
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+mongoose.connect('mongodb://localhost:2701 7/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema=new mongoose.Schema({
+      username:String,
+      password:String
+});
+
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
+
+const User=new mongoose.model("User",userSchema);
+
+app.get('/',function(req,res){
+    res.render('home');
+});
+
+app.get('/login',function(req,res){
+    res.render('login');
+});
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    User.findOne({username:username},function(err,foundUser){
+          if(err)console.log(err);
+          else{
+            if(foundUser){
+              if(foundUser.password===password)res.render('secrets');
+            }
+          }
+    });
+});
+
+app.get('/register',function(req,res){
+    res.render('register');
+});
+
+app.post('/register',function(req,res){
+    const newUser=new User({
+      username:req.body.username,
+      password:req.body.password
+    });
+    newUser.save(function(err){
+      if(err)console.log(err);
+      else res.render('secrets');
+    });
+});
+
+app.get('/logout',function(req,res){
+    res.redirect('/');
+});
+
+app.listen(666,function(){
+    console.log('Server started on port 666');
+});
