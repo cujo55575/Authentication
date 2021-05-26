f152fd1eb2e3a72bf9192b4588d8178ec2782265
cujo55575 <cujo55575@gmail.com>
2021-05-26 20:21:31 +0630

    add md5 hash

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const ejs=require('ejs');
 const mongoose=require('mongoose');
-const encrypt=require('mongoose-encryption');
+const md5=require('md5');
+// const encrypt=require('mongoose-encryption');
 
 const app=express();
 
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
-mongoose.connect('mongodb://localhost:2701 7/userDB', {
+mongoose.connect('mongodb://localhost:27017/userDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -25,7 +26,7 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 
 const User=new mongoose.model("User",userSchema);
 
@@ -39,7 +40,7 @@ app.get('/login',function(req,res){
 
 app.post('/login',function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
     User.findOne({username:username},function(err,foundUser){
           if(err)console.log(err);
           else{
@@ -57,7 +58,7 @@ app.get('/register',function(req,res){
 app.post('/register',function(req,res){
     const newUser=new User({
       username:req.body.username,
-      password:req.body.password
+      password:md5(req.body.password)
     });
     newUser.save(function(err){
       if(err)console.log(err);
